221583e80571b753cb568240517305d21fec7a18
Beniamin <benek9311@wp.pl>
2026-02-20 12:21:52 +0100

    Fixed horizontal scrolling issue in AOS-animated sections (related to 'Projects' section container), modified AOS styles import to SCSS, changed overflow-x: 'hidden' to 'clip' in the body

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -8,37 +8,39 @@ const Projects = () => {
   const isMobileScreen = useMediaQuery({ query: "(max-width: 768px)" });
 
   return (
-    <div className={`container ${scss.wrapper}`} id="projects">
-      <h2 className={scss.title} data-aos="fade-up">
-        Projects
-      </h2>
-      {projects.map(
-        (
-          { id, subtitle, name, description, screenshots, website, code },
-          i,
-        ) => (
-          <div
-            className={scss.project}
-            key={id}
-            data-aos={i % 2 === 0 ? "fade-up-right" : "fade-up-left"}
-          >
-            <ProjectInfo
-              subtitle={subtitle}
-              name={name}
-              description={description}
-              website={website}
-            />
-            <DetailsBox
-              name={name}
-              subtitle={subtitle}
-              screenshots={screenshots}
-              website={website}
-              code={code}
-              isMobileScreen={isMobileScreen}
-            />
-          </div>
-        ),
-      )}
+    <div style={{ overflowX: "clip" }}>
+      <div className={`container ${scss.wrapper}`} id="projects">
+        <h2 className={scss.title} data-aos="fade-up">
+          Projects
+        </h2>
+        {projects.map(
+          (
+            { id, subtitle, name, description, screenshots, website, code },
+            i,
+          ) => (
+            <div
+              className={scss.project}
+              key={id}
+              data-aos={i % 2 === 0 ? "fade-up-right" : "fade-up-left"}
+            >
+              <ProjectInfo
+                subtitle={subtitle}
+                name={name}
+                description={description}
+                website={website}
+              />
+              <DetailsBox
+                name={name}
+                subtitle={subtitle}
+                screenshots={screenshots}
+                website={website}
+                code={code}
+                isMobileScreen={isMobileScreen}
+              />
+            </div>
+          ),
+        )}
+      </div>
     </div>
   );
 };
